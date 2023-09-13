eba5b415341ace7ef59f0ce9de45ec4c91898621
Jett Pennington <jettpennington@gmail.com>
2023-09-13 16:13:15 +0000

    Working clock!!! woo

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
 const clock = document.querySelector('.clock');
 
-var today = new Date();
-var min = "";
-if (today.getMinutes() < 10){
-    min = "0" + today.getMinutes();
-} else {
-    min = today.getMinutes();
+function addZero(num){
+    if (num < 10){
+        return "0" + num;
+    } else {
+        return num;
+    }
 }
-var sec = "";
-if (today.getSeconds() < 10){
-    sec = "0" + today.getSeconds();
-} else {
-    sec = today.getSeconds();
-}
-var time = today.getHours() + ":" + min + ":" + sec;
 
-clock.textContent = time;
+function updateTime(){
+    let today = new Date();
+    
+    let min = addZero(today.getMinutes());
+    let sec = addZero(today.getSeconds());
+    let time = "";
+    let hou = "";
+
+    if(document.getElementById("12").checked){
+        if (today.getHours() > 12){
+            hou = addZero(today.getHours() - 12);
+            time = hou + ":" + min + ":" + sec + " PM";
+        }else {
+            hou = addZero(today.getHours());
+            time = hou + ":" + min + ":" + sec + " AM";
+        }
+    } else{
+        hou = addZero(today.getHours());
+        time = hou + ":" + min + ":" + sec;
+    }
+    clock.textContent = time;
+}
+
+
+
+
+
+setInterval(updateTime, 1000);
+
