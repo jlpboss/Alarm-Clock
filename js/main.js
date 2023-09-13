ec6c1d023a6a1e67f884436cf57db15cba46b2e8
Jett Pennington <jettpennington@gmail.com>
2023-09-13 15:32:23 +0000

    added time and logic to make it always 2 digits

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+const clock = document.querySelector('.clock');
 
+var today = new Date();
+var min = "";
+if (today.getMinutes() < 10){
+    min = "0" + today.getMinutes();
+} else {
+    min = today.getMinutes();
+}
+var sec = "";
+if (today.getSeconds() < 10){
+    sec = "0" + today.getSeconds();
+} else {
+    sec = today.getSeconds();
+}
+var time = today.getHours() + ":" + min + ":" + sec;
+
+clock.textContent = time;
